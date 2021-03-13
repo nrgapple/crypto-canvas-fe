c784db912c6d93581525399c136ac8934b06991d
AG <champtoday@gmail.com>
2021-03-13 15:21:19 -0500

    ✨ Move index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,134 +1,53 @@
-import React, { useEffect, useState } from "react";
-import Layout from "../components/Layout";
-import SidePanel from "../components/SidePanel";
-import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
-import {
-  selectedExhibitState,
-  selectedPixelsState,
-  worldState,
-} from "../state";
+import { Box, Square } from "@chakra-ui/layout";
+import { Skeleton } from "@chakra-ui/skeleton";
 import dynamic from "next/dynamic";
-import { useWeb3 } from "../hooks/useWeb3";
-import { usePixels } from "../hooks/usePixels";
-import { Pixel, WorldStateType } from "../interfaces";
-import ExhibitDetailPanel from "../components/ExhibitDetailPanel";
-import Link from "next/link";
-import { GetServerSideProps } from "next";
 import { useRouter } from "next/router";
+import { useEffect, useRef, useState } from "react";
+import { useRecoilState, useRecoilValue } from "recoil";
+import Layout from "../components/Layout";
+import { usePixels } from "../hooks/usePixels";
+import { useWeb3 } from "../hooks/useWeb3";
+import { selectedExhibitState } from "../state";
 
 const World = dynamic(() => import("../components/World"), {
   ssr: false,
 });
 
-interface DataProps {
-  exhibitId?: number;
-}
-
-const HomePage = ({ exhibitId }: DataProps) => {
+const Home = () => {
   const { loading, web3Contract } = useWeb3();
-  const { checkout, update } = usePixels(web3Contract);
-  const [world, setWorld] = useRecoilState(worldState);
-  const setSelectedPixels = useSetRecoilState(selectedPixelsState);
+  usePixels(web3Contract);
   const [selectedExhibit, setSelectedExhibit] = useRecoilState(
     selectedExhibitState
   );
   const router = useRouter();
 
-  const handleCheckout = async (selected: Pixel[]) => {
-    try {
-      await checkout(selected);
-      setSelectedPixels([]);
-      setWorld(WorldStateType.view);
-    } catch (e) {}
-  };
-
-  const refresh = () => {
-    update();
-  };
-
   useEffect(() => {
-    setSelectedExhibit(exhibitId);
-  }, [exhibitId]);
-
-  useEffect(() => {
-    if (selectedExhibit !== undefined) {
-      router.replace({
-        query: { exhibit: selectedExhibit.toString() },
-      });
-    } else {
-      router.replace({ query: {} });
+    if (selectedExhibit != undefined) {
+      setSelectedExhibit(undefined);
+      router.push(`/exhibit/${selectedExhibit}`);
     }
   }, [selectedExhibit]);
 
   return (
-    <Layout title="Crypto Canvas">
-      <div className="nav-bar">
-        <div className="flex-center-baseline">
-          <h5>Crypto Canvas</h5>
-          <Link href={"/about"}>
-            <h6 className="clickable">about</h6>
-          </Link>
-          <Link href={"/bids"}>
-            <h6 className="clickable">bids</h6>
-          </Link>
-        </div>
-        <div
-          className="button"
-          onClick={() =>
-            setWorld(
-              world !== WorldStateType.view
-                ? WorldStateType.view
-                : WorldStateType.create
-            )
-          }
-        >
-          {world === WorldStateType.view ? "Create" : "Cancel"}
-        </div>
-      </div>
-      {loading && !web3Contract ? (
-        <div className="flex-center-center">
-          <h3>Loading Web3</h3>
-        </div>
-      ) : (
-        <>
-          {!loading && web3Contract != undefined && (
-            <div className="flex-space-strech m8">
-              <div className="frame">
-                <div className="border">
-                  <World you={web3Contract?.accounts[0] ?? ""} />
-                </div>
-              </div>
-              {world !== WorldStateType.create &&
-                selectedExhibit != undefined && (
-                  <ExhibitDetailPanel
-                    web3Contract={web3Contract}
-                    onRefresh={refresh}
-                  />
-                )}
-              {world === WorldStateType.create && (
-                <SidePanel onCheckout={handleCheckout} />
-              )}
-            </div>
-          )}
-        </>
-      )}
+    <Layout>
+      <Square w="100%" h="100%" padding="8px">
+        {loading && !web3Contract !== undefined ? (
+          <Skeleton />
+        ) : (
+          <Box
+            flexBasis="800px"
+            h="100%"
+            p="8px"
+            borderRadius="4px"
+            border="1px solid var(--border)"
+          >
+            {/* <Box background="#000" h="100%" w="100%" /> */}
+            <World you={web3Contract?.accounts[0] ?? ""} />
+          </Box>
+        )}
+      </Square>
     </Layout>
   );
 };
 
-export default HomePage;
-
-export const getServerSideProps: GetServerSideProps<DataProps> = async ({
-  query,
-}) => {
-  if (query && query.exhibit) {
-    return {
-      props: {
-        exhibitId: parseInt(query.exhibit as string),
-      } as DataProps,
-    };
-  }
-  return {
-    props: {},
-  };
-};
+export default Home;
