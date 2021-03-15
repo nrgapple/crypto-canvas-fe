664fc36fc6a09b3c97a1264fe5d108b3228efbee
AG <champtoday@gmail.com>
2021-03-15 16:53:01 -0400

    ✨ create poc endpoint to get the pixels from the chain

--- app.config.js
+++ app.config.js
@@ -1,3 +1,5 @@
 export const config = {
   contractAddress: "0x00EbDb4b33c21f4b1F6c43852840Df8207bdBBF7",
+  infuraProviderUri:
+    "https://rinkeby.infura.io/v3/566baaea6f1246d8b5e3762f833bc321",
 };
