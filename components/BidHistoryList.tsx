36d1761007b67e3f725a174c6e79b50f123c6d21
AG <champtoday@gmail.com>
2021-03-19 16:59:02 -0400

    🔧 Remove old contract

--- components/BidHistoryList.tsx
+++ components/BidHistoryList.tsx
@@ -1,50 +1,36 @@
 import { Table, Tbody, Th, Thead, Tr, Td } from "@chakra-ui/table";
 import React, { useEffect, useState } from "react";
-import { useWeb3 } from "../hooks/useWeb3";
 import { Bid } from "../interfaces";
 import { ETH_SYMBOL } from "../utils/helpers";
 
-
 interface Props {
-    exhibitId: number
+  exhibitId: number;
 }
 
-const BidHistoryList = ({exhibitId}: Props) => {
-    const [highestBids, setHighestBids] = useState<Bid[] | undefined>()
-    const {web3Contract} = useWeb3()
+const BidHistoryList = ({ exhibitId }: Props) => {
+  const [highestBids, setHighestBids] = useState<Bid[] | undefined>();
 
-    useEffect(() => { 
-        if (web3Contract && exhibitId !== undefined) {
-            ;(async () => {
-               const bids = await web3Contract.contract.methods.getBidHistoryForExhibit(exhibitId).call();
-               bids && setHighestBids(bids.map((bid: any) => ({
-                   from: bid.fromAddress,
-                   amount: parseFloat(web3Contract.web3.utils.fromWei(bid.amount))
-               } as Bid)))
-            })()
-        }
-    }, [web3Contract, exhibitId])
-
-    return (
-        <Table>
-            <Thead>
-                <Tr>
-                    <Th>Price</Th>
-                    <Th>From</Th>
-                    <Th>Status</Th>
-                </Tr>
-            </Thead>
-            <Tbody>
-            {highestBids && highestBids.map((bid, i) => (
+  return (
+    <Table>
+      <Thead>
+        <Tr>
+          <Th>Price</Th>
+          <Th>From</Th>
+          <Th>Status</Th>
+        </Tr>
+      </Thead>
+      <Tbody>
+        {highestBids &&
+          highestBids.map((bid, i) => (
             <Tr key={i}>
-                <Td>{`${ETH_SYMBOL}${bid.amount}`}</Td>
-                <Td>{bid.from}</Td>
-                <Td>PENDING</Td>
+              <Td>{`${ETH_SYMBOL}${bid.amount}`}</Td>
+              <Td>{bid.from}</Td>
+              <Td>PENDING</Td>
             </Tr>
-            ))}
-            </Tbody>
-        </Table>
-    )
-}
+          ))}
+      </Tbody>
+    </Table>
+  );
+};
 
 export default BidHistoryList;
