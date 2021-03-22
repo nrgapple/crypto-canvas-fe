47c97a07d01685722513e13aec31937c8b4bcf0a
AG <champtoday@gmail.com>
2021-03-21 22:27:02 -0400

    🔧 Try to fix cors?

--- pages/api/darts/image/[dartId]/index.ts
+++ pages/api/darts/image/[dartId]/index.ts
@@ -6,9 +6,9 @@ export default async function handle(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  NextCors(req, res, {
+  await NextCors(req, res, {
     // Options
-    methods: ["GET"],
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
     origin: "*",
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   });
