605f0d220a35cd4ab7e372a7f55831e42925437b
AG <champtoday@gmail.com>
2021-03-24 15:21:24 -0400

    ✨ Add more to landing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { Wrap, Heading, VStack, Image, Center } from "@chakra-ui/react";
+import { Wrap, Heading, VStack, Image, Center, Text } from "@chakra-ui/react";
 import React from "react";
 import Layout from "../components/Layout";
 
@@ -19,23 +19,47 @@ const Home = () => {
             <Heading as="h4" size="lg">
               Other NFTs
             </Heading>
-            <Image src="/images/theirs.png" w="100%" />
+            <Image src="/images/theirs-pic.png" w="100%" />
           </VStack>
           <VStack w="500px">
             <Heading as="h4" size="lg">
               De-Art
             </Heading>
-            <Image src="/images/ours.png" w="100%" />
+            <Image src="/images/ours-pic.png" w="100%" />
           </VStack>
         </Wrap>
         <Wrap justify="center" p="16px">
           <Center w="500px">
-            <Heading as="h4" size="md" p="16px">
-              What will you do when your NFT Provider shuts down?
-            </Heading>
+            <VStack>
+              <Heading as="h4" size="lg" p="16px">
+                What will you do when your NFT Provider shuts down?
+              </Heading>
+              <Text>
+                If the site you own your NFT on goes down you loose everything.
+                Yes you can prove that you own some entity.{" "}
+                <strong>But there is no proof of that entity's data.</strong>
+              </Text>
+            </VStack>
           </Center>
           <VStack w="500px">
             <Image src="/images/server-down.png" w="100%" />
+          </VStack>
+        </Wrap>
+
+        <Wrap justify="center" p="16px">
+          <Center w="500px">
+            <VStack>
+              <Heading as="h4" size="lg" p="16px">
+                Your Art Exists everywhere
+              </Heading>
+              <Text>
+                With De-Art your image will exist forever on the blockchain
+                along with the proof of ownership.
+              </Text>
+            </VStack>
+          </Center>
+          <VStack w="500px">
+            <Image src="/images/ours-everywhere.png" w="100%" />
           </VStack>
         </Wrap>
       </VStack>
