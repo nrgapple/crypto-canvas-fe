33c1e63904602918f9e4feb92021ac8c686ceb98
AG <champtoday@gmail.com>
2021-03-28 13:34:35 -0400

    ✨ Fix image buffer fetch api call

--- hooks/useImageBuffer.ts
+++ hooks/useImageBuffer.ts
@@ -4,7 +4,7 @@ import { bufferToHex } from "../utils/helpers";
 export const useImageBuffer = (dartId?: number) => {
   const buffer = useAsync(async () => {
     if (dartId === undefined) return undefined;
-    const resp = await fetch(`api/darts/buffer/${dartId}`);
+    const resp = await fetch(`/api/darts/buffer/${dartId}`);
     const array = (await resp.body?.getReader().read())!.value;
     return array;
   }, [dartId]);
