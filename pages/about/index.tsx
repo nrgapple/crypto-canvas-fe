2a70ef37e5794132ec31909eb4c54929468397b6
Matt <mjruddy94@gmail.com>
2021-03-29 20:31:02 -0400

    ✨ add about page

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,52 +1,75 @@
 import Layout from "../../components/Layout";
 import React from "react";
-import Link from "next/link";
+import {
+  Center,
+  Divider,
+  Heading,
+  Link,
+  Text,
+  VStack,
+  Wrap,
+} from "@chakra-ui/layout";
+import { ExternalLinkIcon } from "@chakra-ui/icons";
+import { Image } from "@chakra-ui/image";
+import NextLink from "next/link";
 
 const About = () => {
   return (
     <Layout title="About">
-      <div className="nav-bar">
-        <div className="flex-center-baseline">
-          <Link href={"/"}>
-            <h5 className="clickable">Crypto Canvas</h5>
-          </Link>
-          <Link href={"/about"}>
-            <h6 className="clickable">about</h6>
-          </Link>
-          <Link href={"/bids"}>
-            <h6 className="clickable">bids</h6>
-          </Link>
-        </div>
-      </div>
-      <div className="m8">
-        <h1>Create, Buy, and Sell Art on a Global Canvas</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae
-          congue eu consequat ac felis donec et. Rhoncus mattis rhoncus urna
-          neque. Feugiat in fermentum posuere urna nec tincidunt praesent.
-          Venenatis tellus in metus vulputate eu. Porttitor eget dolor morbi non
-          arcu risus quis. Pellentesque elit ullamcorper dignissim cras
-          tincidunt lobortis. Purus non enim praesent elementum facilisis leo.
-          Faucibus nisl tincidunt eget nullam. Nulla aliquet porttitor lacus
-          luctus accumsan tortor posuere ac. Tempor nec feugiat nisl pretium
-          fusce id velit ut. Eget mauris pharetra et ultrices neque ornare. Enim
-          sed faucibus turpis in eu mi. Et leo duis ut diam quam. Sem nulla
-          pharetra diam sit amet nisl suscipit adipiscing bibendum. Est sit amet
-          facilisis magna etiam tempor orci. Non pulvinar neque laoreet
-          suspendisse. Dignissim enim sit amet venenatis. Nunc sed id semper
-          risus in hendrerit gravida rutrum. Commodo elit at imperdiet dui
-          accumsan. Pellentesque pulvinar pellentesque habitant morbi tristique
-          senectus. Morbi tristique senectus et netus et malesuada fames ac
-          turpis. Aliquam eleifend mi in nulla posuere sollicitudin aliquam
-          ultrices. Adipiscing bibendum est ultricies integer quis auctor elit
-          sed. Sed augue lacus viverra vitae congue eu. Porttitor lacus luctus
-          accumsan tortor posuere ac ut. Odio pellentesque diam volutpat commodo
-          sed egestas egestas fringilla phasellus. Vel quam elementum pulvinar
-          etiam non quam. Urna condimentum mattis pellentesque id nibh tortor
-          id.
-        </p>
-      </div>
+      <VStack p="16px" overflowY="scroll" w="100%" h="100%">
+        <Heading>
+          Digital Assets Owned by{" "}
+          <span style={{ fontWeight: "normal" }}>You</span>
+        </Heading>
+        <Text as="i" p="16px">
+          <Link
+            href="https://docs.openzeppelin.com/contracts/3.x/erc721"
+            isExternal
+          >
+            <strong>ERC721</strong>
+            <ExternalLinkIcon mx="2px" />
+          </Link>{" "}
+          is a specific smart contract which can be deployed to the Ethereum
+          Blockchain that allows individuals to mint items (NFTs) containing an
+          owner and a small amount of metadata.
+        </Text>
+        <Divider />
+        <Center>
+          <Heading fontWeight="normal">Others</Heading>
+        </Center>
+        <Text p="16px">
+          Since the amount of metadata needs to be small, what other NFTs are
+          doing at the moment is actually storing the real data{" "}
+          <strong>off-chain</strong> and then only having a reference to the
+          actual data on the Blockchain. Off-chain is just a fancy word for
+          <strong> centralized</strong> and it really defeats the entire purpose
+          of a digital asset that you own. If this "off-chain" server was ever
+          hacked or taken down, your digital asset is gone.
+        </Text>
+        <Center>
+          <Heading fontWeight="normal">Us</Heading>
+        </Center>
+        <Text p="16px">
+          We understand that there are limitations with the Blockchain at the
+          moment. Storage is very difficult and our goal is to create a 100%
+          decentralized digital asset where all of the data is stored directly
+          in the Blockchain itself rather than some centralized server. This
+          means when the NFT is created with an image, it will for ever be
+          engraved into the blockchain. deART is for people who want to truly
+          own every part of the digital asset they create.
+        </Text>
+        <Heading fontWeight="normal">Creators</Heading>
+        <Wrap justifyContent="center">
+          <VStack>
+            <Image h="200px" src={`/api/darts/image/12`} borderRadius="3px" />
+            <NextLink href="/browse/12">deART#12</NextLink>
+          </VStack>
+          <VStack>
+            <Image h="200px" src={`/api/darts/image/13`} borderRadius="3px" />
+            <NextLink href="/browse/13">deART#13</NextLink>
+          </VStack>
+        </Wrap>
+      </VStack>
     </Layout>
   );
 };
